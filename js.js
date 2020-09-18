d61242fd5c9e815c8dc028866a0925af36a09bf2
gabrielandre-andre <silvagabrielandre7@gmail.com>
2020-09-18 13:18:48 +0000

    :)

--- js.js
+++ js.js
@@ -105,10 +105,56 @@ const content = {}
 
 
     function passwordVerify(){
-        content.passwordPrincipalSection.style.display = 'block'
+
+
+        if(valor_cookie('newAcount') == 1){
+            alert('ok')
+        }else{
+            content.passwordPrincipalSection.style.display = 'block'
+        }
+        
     }
 
 
     function closeAba(){
         content.passwordPrincipalSection.style.display = 'none'
     }
+
+
+
+
+    function SaveMyPersonalPassword(){
+        var MypersonalPassword = document.getElementById('passwordCok').value
+        document.cookie = 'newAcount = 1'
+        content.passwordPrincipalSection.style.display = 'none'
+
+    }
+
+    function valor_cookie(nome_cookie) {
+    // Adiciona o sinal de = na frente do nome do cookie
+    var cname = ' ' + nome_cookie + '=';
+    
+    // Obtém todos os cookies do documento
+    var cookies = document.cookie;
+    
+    // Verifica se seu cookie existe
+    if (cookies.indexOf(cname) == -1) {
+        return false;
+    }
+    
+    // Remove a parte que não interessa dos cookies
+    cookies = cookies.substr(cookies.indexOf(cname), cookies.length);
+
+    // Obtém o valor do cookie até o ;
+    if (cookies.indexOf(';') != -1) {
+        cookies = cookies.substr(0, cookies.indexOf(';'));
+    }
+    
+    // Remove o nome do cookie e o sinal de =
+    cookies = cookies.split('=')[1];
+    
+    // Retorna apenas o valor do cookie
+    return decodeURI(cookies);
+    }
+
+    console.log(valor_cookie('newAcount'))
